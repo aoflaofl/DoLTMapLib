e6333578c5b09d2a6c0bc429113ec40cccc9aa1e
Gene Johannsen <aofl@yahoo.com>
2015-12-17 16:50:48 -0800

    Add differential values to Directions

--- src/main/java/com/spamalot/dolt/map/Direction.java
+++ src/main/java/com/spamalot/dolt/map/Direction.java
@@ -10,17 +10,47 @@ enum Direction {
   /**
    * Up.
    */
-  UP,
+  UP(0, -1),
   /**
    * Down.
    */
-  DOWN,
+  DOWN(0, 1),
   /**
    * Left.
    */
-  LEFT,
+  LEFT(-1, 0),
   /**
    * Lef --- uh, I mean Right.
    */
-  RIGHT
+  RIGHT(1, 0);
+
+  public int gethDiff() {
+    return hDiff;
+  }
+
+  private void sethDiff(final int hDiff) {
+    this.hDiff = hDiff;
+  }
+
+  public int getvDiff() {
+    return vDiff;
+  }
+
+  private void setvDiff(final int vDiff) {
+    this.vDiff = vDiff;
+  }
+
+  /**
+   * Differential for moving in the horizontal direction.
+   */
+  private int hDiff;
+  /**
+   * Differential for moving in the vertical direction.
+   */
+  private int vDiff;
+
+  Direction(final int hDiff, final int vDiff) {
+    sethDiff(hDiff);
+    setvDiff(vDiff);
+  }
 }
