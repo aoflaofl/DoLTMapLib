bef2cae60cb708b25db87e7582a074c39898de52
Gene Johannsen <aofl@yahoo.com>
2019-12-04 17:10:03 -0800

    Start adding tests.

---
+++ src/test/java/com/spamalot/dolt/map/MapTileTest.java
@@ -0,0 +1,232 @@
+/**
+ * 
+ */
+package com.spamalot.dolt.map;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author gejohann
+ *
+ */
+public class MapTileTest {
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#MapTile(com.spamalot.dolt.map.MapTile.MapTileType)}.
+   */
+  @Test
+  public void testMapTile() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#linkTileInDirection(com.spamalot.dolt.map.Direction, com.spamalot.dolt.map.MapTile)}.
+   */
+  @Test
+  public void testLinkTileInDirection() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#get(com.spamalot.dolt.map.Direction)}.
+   */
+  @Test
+  public void testGet() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#getAdjacentWaterTiles()}.
+   */
+  @Test
+  public void testGetAdjacentWaterTiles() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#getDown()}.
+   */
+  @Test
+  public void testGetDown() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#getRandomAdjacentWaterTile()}.
+   */
+  @Test
+  public void testGetRandomAdjacentWaterTile() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#getRight()}.
+   */
+  @Test
+  public void testGetRight() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#getTerritory()}.
+   */
+  @Test
+  public void testGetTerritory() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#getType()}.
+   */
+  @Test
+  public void testGetType() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#isInSameTerritory(com.spamalot.dolt.map.MapTile)}.
+   */
+  @Test
+  public void testIsInSameTerritory() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#isOffLimits()}.
+   */
+  @Test
+  public void testIsOffLimits() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#setOffLimits(boolean)}.
+   */
+  @Test
+  public void testSetOffLimits() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#setTerritory(com.spamalot.dolt.map.Territory)}.
+   */
+  @Test
+  public void testSetTerritory() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#setType(com.spamalot.dolt.map.MapTile.MapTileType)}.
+   */
+  @Test
+  public void testSetType() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link com.spamalot.dolt.map.MapTile#toString()}.
+   */
+  @Test
+  public void testToString() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#Object()}.
+   */
+  @Test
+  public void testObject() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#getClass()}.
+   */
+  @Test
+  public void testGetClass() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#hashCode()}.
+   */
+  @Test
+  public void testHashCode() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+   */
+  @Test
+  public void testEquals() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#clone()}.
+   */
+  @Test
+  public void testClone() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#toString()}.
+   */
+  @Test
+  public void testToString1() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#notify()}.
+   */
+  @Test
+  public void testNotify() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#notifyAll()}.
+   */
+  @Test
+  public void testNotifyAll() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#wait(long)}.
+   */
+  @Test
+  public void testWaitLong() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#wait(long, int)}.
+   */
+  @Test
+  public void testWaitLongInt() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#wait()}.
+   */
+  @Test
+  public void testWait() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#finalize()}.
+   */
+  @Test
+  public void testFinalize() {
+    fail("Not yet implemented");
+  }
+
+}
