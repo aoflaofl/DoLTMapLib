9ba6d486dfff6749b2529297632c865c86906c12
Gene Johannsen <aofl@yahoo.com>
2020-01-14 12:26:10 -0800

    Initial Commit of CLI package

---
+++ dolt-map-cli/src/main/java/com/spamalot/dolt/cli/DoLTCLI.java
@@ -0,0 +1,11 @@
+package com.spamalot.dolt.cli;
+
+import com.spamalot.dolt.map.DoltWorld;
+
+public class DoLTCLI {
+
+  public static void main(String[] args) {
+    final DoltWorld world = new DoltWorld(0, 0, 0);
+  }
+
+}
