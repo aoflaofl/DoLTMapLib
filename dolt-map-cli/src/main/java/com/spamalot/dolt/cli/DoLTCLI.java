ccce21bca123e8c89344f8cab2023993c86f822a
Gene Johannsen <aofl@yahoo.com>
2020-01-16 17:17:57 -0800

    More refactoring.  Doesn't work.

--- dolt-map-cli/src/main/java/com/spamalot/dolt/cli/DoLTCLI.java
+++ dolt-map-cli/src/main/java/com/spamalot/dolt/cli/DoLTCLI.java
@@ -1,15 +1,34 @@
 package com.spamalot.dolt.cli;
 
+import com.spamalot.dolt.map.DoltMap;
 import com.spamalot.dolt.world.DoltWorld;
 import com.spamalot.dolt.world.WorldTile;
 import com.spamalot.dolt.world.WorldTileType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DoLTCLI {
+  /** Loggit. */
+  private static final Logger LOGGER = LoggerFactory.getLogger(DoLTCLI.class);
+  /** Number of Territories in a default Map. */
+  private static final int DEFAULT_NUM_TERRITORIES = 10;
+
+  /** Height of a default Map. */
+  private static final int DEFAULT_MAP_HEIGHT = 20;
+
+  /** Width of a default Map. */
+  private static final int DEFAULT_MAP_WIDTH = 40;
+
   public static void main(String[] args) {
     final DoltWorld world = new DoltWorld(10, 10);
     WorldTile t = world.getMapTile(5, 5);
     t.setType(WorldTileType.LAND);
 
     System.out.println(world.toString());
+
+    LOGGER.info("Something will happen here!");
+    final DoltMap world1 = new DoltMap(DEFAULT_MAP_WIDTH, DEFAULT_MAP_HEIGHT, DEFAULT_NUM_TERRITORIES);
+
+    LOGGER.info("World: {}", world1);
   }
 }
